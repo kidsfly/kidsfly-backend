02d4a6404bc20e7a5ab30ce49977249b8c88bef5
BrandCam <BrandCam>
2019-04-25 11:50:30 -0500

    should set up db on deploy now

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,11 +4,9 @@ const environment = process.env.NODE_ENV || "production";
 const db = knex(knexConfig[environment]);
 
 // uncomment to reset server on every reload
-if (process.env.NODE_ENV === "production") {
-  db.migrate
-    .rollback()
-    .then(() => db.migrate.latest())
-    .then(() => db.seed.run());
-}
+db.migrate
+  .rollback()
+  .then(() => db.migrate.latest())
+  .then(() => db.seed.run());
 
 module.exports = db;
