4b008143077a109eecd5efaa51c499bff605e330
BrandCam <BrandCam>
2019-04-24 19:56:56 -0500

    this will maybe work

--- knexfile.js
+++ knexfile.js
@@ -35,9 +35,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      database: process.env.DATABASE_URL,
-      user: "username",
-      password: "password"
+      database: process.env.DATABASE_URL
     },
     pool: {
       min: 2,
